edcc5727cc64f8d0808f983dc95f718b57cc5b5f
Valeria Montejo <vaal.moon@hotmail.es>
2025-12-09 16:46:00 +0100

    preprocess: start modifying preprocess.py file

---
+++ Lunar-crater-age-logic/preprocess.py
@@ -0,0 +1,6 @@
+"""
+Lunar Crater Age classification - Data Preprocessing Pipeline
+
+This module contains all functions needed to load, preprocess, and batch
+lunar crater images for training a CNN model
+"""
